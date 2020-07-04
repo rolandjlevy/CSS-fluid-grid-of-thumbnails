7853a34155da85b60bea561a8399aba6eb403ac7
Roland Levy <rolandjlevy@gmail.com>
2020-07-04 19:29:33 +0000

    Tried to add html2canvas to each thumb

--- script.js
+++ script.js
@@ -2,10 +2,15 @@
 const card = document.querySelector('.card');
 const container = document.querySelector('.card-container');
 
-function createCard() {
+function createCard(counter) {
   const clonedCard = card.cloneNode(true);
+  const imgName = 'image-' + counter;
   clonedCard.style.backgroundImage = getRandomImage();
   clonedCard.style.display = 'block';
+  clonedCard.id = imgName;
+  // clonedCard.addEventListener('click', (e) => {
+  //   captureThenDownload(imgName, imgName + '.png')
+  // });
   container.appendChild(clonedCard);
 }
 
@@ -14,10 +19,29 @@ function getRandomImage() {
   return `url('https://source.unsplash.com/random/${randomNum}')`;
 }
 
-let images = 32;
+let counter = 32;
 
-while (images > 0) {
-  createCard();
-  images--;
+while (counter > 0) {
+  createCard(counter);
+  counter--;
 }
 
+function captureThenDownload(divName, outputFilename) {
+  window.scrollTo(0, 0);
+  const options = {
+    logging: true, 
+    letterRendering: 1,
+    allowTaint: false, 
+    removeContainer: true,
+    useCORS: true,
+    scrollX: 0,
+    scrollY: 0
+  };
+  const div = document.querySelector("#" + divName);
+  html2canvas(div, options).then(canvas => {
+    const a = document.createElement('a');
+    a.href = canvas.toDataURL('image/png', 0.9);
+    a.download = outputFilename;
+    a.click();
+  });
+}
